c190767f195efc28c5e6a593f4e61d645a5f3b66
boss <372506589@qq.com>
2019-02-14 18:28:57 +0800

    test1

--- src/main/java/com/hhb/services/DemoUserServiceImpl.java
+++ src/main/java/com/hhb/services/DemoUserServiceImpl.java
@@ -4,6 +4,8 @@ import com.hhb.dao.DemoUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Transient;
+
 @Service
 public class DemoUserServiceImpl implements DemoUserService {
 
